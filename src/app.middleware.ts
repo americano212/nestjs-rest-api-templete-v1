0459ec065b9e90399d98ff99b52dd04b3a6f8c1d
Dongjun Shin <53344349+wq0212@users.noreply.github.com>
2023-10-13 08:14:50 +0000

    feat: setting main.ts with middleware

---
+++ src/app.middleware.ts
@@ -0,0 +1,30 @@
+import type { INestApplication } from '@nestjs/common';
+import compression from 'compression';
+import session from 'express-session';
+import helmet from 'helmet';
+import passport from 'passport';
+
+export function middleware(app: INestApplication): INestApplication {
+  const isProduction = process.env['NODE_ENV'] === 'production';
+
+  app.use(compression());
+  app.use(
+    session({
+      secret: 'tEsTeD',
+      resave: false,
+      saveUninitialized: true,
+      cookie: { secure: isProduction },
+    }),
+  );
+  app.use(passport.initialize());
+  app.use(passport.session());
+
+  app.use(
+    helmet({
+      contentSecurityPolicy: isProduction ? undefined : false,
+      crossOriginEmbedderPolicy: isProduction ? undefined : false,
+    }),
+  );
+
+  return app;
+}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,33 @@
+import { Logger as NestLogger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(8081);
+import { middleware } from './app.middleware';
+import type { NestExpressApplication } from '@nestjs/platform-express';
+
+async function bootstrap(): Promise<string> {
+  const isProduction = process.env['NODE_ENV'] === 'production';
+  const app = await NestFactory.create<NestExpressApplication>(AppModule, {
+    bufferLogs: true,
+  });
+  if (isProduction) {
+    app.enable('trust proxy');
+  }
+  middleware(app);
+
+  app.enableCors();
+  await app.listen(process.env['PORT'] || 3000);
+
+  return app.getUrl();
 }
-bootstrap();
+
+void (async (): Promise<void> => {
+  try {
+    const url = await bootstrap();
+    NestLogger.log(url, 'Bootstrap');
+    console.log('[SUCCESS]', url);
+  } catch (error) {
+    NestLogger.error(error, 'Bootstrap');
+    console.log('[ERROR]', error);
+  }
+})();
