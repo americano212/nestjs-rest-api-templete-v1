6fc1a0423cbd8285e1ce22baf2cccf41d5112ccc
Dongjun Shin <53344349+wq0212@users.noreply.github.com>
2023-10-14 07:47:08 +0000

    feat: Add core entity for extend other entities

---
+++ src/entities/core.entity.ts
@@ -0,0 +1,25 @@
+import {
+  CreateDateColumn,
+  DeleteDateColumn,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+import { ApiProperty } from '@nestjs/swagger';
+
+export class CoreEntity {
+  @ApiProperty({ example: 1, description: 'primary key' })
+  @PrimaryGeneratedColumn({ type: 'int', unsigned: true, name: 'id' })
+  id!: number;
+
+  @ApiProperty({ example: new Date(), description: '생성 일자' })
+  @CreateDateColumn()
+  created_at!: Date;
+
+  @ApiProperty({ example: new Date(), description: '수정 일자' })
+  @UpdateDateColumn()
+  updated_at!: Date;
+
+  @ApiProperty({ example: new Date(), description: '삭제 일자' })
+  @DeleteDateColumn()
+  deleted_at?: Date | null;
+}
