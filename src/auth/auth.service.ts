75016f824e345487af7cf74867b5685c1a255248
Dongjun Shin <53344349+wq0212@users.noreply.github.com>
2023-10-21 16:10:29 +0000

    refactor: Erase unused code in auth service

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -3,23 +3,11 @@ import { Injectable } from '@nestjs/common';
 import * as bcrypt from 'bcrypt';
 import { User, UsersRepository } from 'src/shared/user';
 
-import { LocalLoginDto } from './dto';
-import { JwtSign } from '.';
-
 // https://soraji.github.io/back/2022/12/15/sociallogin/
 
 @Injectable()
 export class AuthService {
   constructor(private readonly usersRepository: UsersRepository) {}
-
-  public async localLogin(loginData: LocalLoginDto): Promise<JwtSign | null> {
-    const user = await this.usersRepository.getByUsername(loginData.username);
-    console.log(user);
-    if (!user) return null;
-    // TODO add logic
-
-    return { access_token: 'test', refresh_token: 'test' };
-  }
 
   public async validateUser(
     username: string,
