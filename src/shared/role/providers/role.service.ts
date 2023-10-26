000816176faa34d31ec3e439c97f18cc5425ad74
Dongjun Shin <53344349+wq0212@users.noreply.github.com>
2023-10-26 16:26:54 +0000

    refactor: Seperate service that add Role to User

--- src/shared/role/providers/role.service.ts
+++ src/shared/role/providers/role.service.ts
@@ -1,6 +1,25 @@
+import { User } from '#entities/user.entity';
 import { Injectable } from '@nestjs/common';
+import { RolesRepository } from './role.repository';
+import { UserRolesRepository } from './user-role.repository';
 
 @Injectable()
 export class RoleService {
+  constructor(
+    private readonly rolesRepository: RolesRepository,
+    private readonly userRolesRepository: UserRolesRepository,
+  ) {}
   // TODO addRole 추가
+  public async addRoleToUser(role_name: string, user: User): Promise<boolean> {
+    try {
+      // TODO role이 없을 때
+      const role = await this.rolesRepository.findRoleByName(role_name);
+      await this.userRolesRepository.create({ user, role });
+      return true;
+    } catch (error) {
+      // TODO exception 처리 필요
+      console.log(error);
+      return false;
+    }
+  }
 }
--- src/shared/user/user.service.ts
+++ src/shared/user/user.service.ts
@@ -4,20 +4,18 @@ import * as bcrypt from 'bcrypt';
 import { LocalRegisterDto, UserDto } from './dto';
 import { UsersRepository } from './user.repository';
 import { UtilService } from 'src/common';
-import { RolesRepository, UserRolesRepository } from '../role/providers';
 import { QueryFailedError } from 'typeorm';
+import { RoleService } from '../role/providers';
 
 @Injectable()
 export class UserService {
   constructor(
     private readonly usersRepository: UsersRepository,
-    private readonly rolesRepository: RolesRepository,
-    private readonly userRolesRepository: UserRolesRepository,
     private readonly util: UtilService,
+    private readonly role: RoleService,
   ) {}
 
   public async create(userData: LocalRegisterDto): Promise<boolean> {
-    // TODO 트랜잭션 처리 필요
     const { password, ...userDataExceptPassword } = userData;
     const passwordHash = await this.util.passwordEncoding(password);
 
@@ -26,24 +24,21 @@ export class UserService {
         passwordHash,
         ...userDataExceptPassword,
       });
-
-      console.log('user', user);
       const roles = userData.roles;
-      console.log(roles);
-      // TODO addRole로 때어내기
-      // TODO role이 없을 때
+
       for (let i = 0; i < roles?.length; i++) {
         const role_name = roles[i];
-        const role = await this.rolesRepository.findRoleByName(role_name);
-        const user_role = await this.userRolesRepository.create({ user, role });
-        console.log('user_role', user_role);
+        await this.role.addRoleToUser(role_name, user);
       }
-      // TODO 리턴값 고민 필요.
       return true;
     } catch (error: unknown) {
       if (error instanceof QueryFailedError) {
+        // TODO error Enum화
         console.log('errno', error?.driverError.code);
-        throw new HttpException('사용자 이미 존재', HttpStatus.BAD_REQUEST);
+        throw new HttpException(
+          `User's Email already exists`,
+          HttpStatus.BAD_REQUEST,
+        );
       }
       return false;
     }
