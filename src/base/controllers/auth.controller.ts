729318c63c3f54d8c908e9b599d4247fbdc7d9e5
Dongjun Shin <53344349+wq0212@users.noreply.github.com>
2023-10-29 15:28:43 +0000

    feat: update login that local to jwt

--- src/base/controllers/auth.controller.ts
+++ src/base/controllers/auth.controller.ts
@@ -1,7 +1,7 @@
 import { Controller, Get, Post, Req, Res, UseGuards } from '@nestjs/common';
 import { ApiBody, ApiTags } from '@nestjs/swagger';
 
-import { LocalLoginGuard, Payload } from '../../auth';
+import { AuthService, JwtSign, LocalLoginGuard, Payload } from '../../auth';
 import { LocalLoginDto } from '../dto';
 import { ReqUser } from '../../common';
 import { Request, Response } from 'express';
@@ -9,11 +9,13 @@ import { Request, Response } from 'express';
 @ApiTags('Auth')
 @Controller()
 export class AuthController {
+  constructor(private readonly auth: AuthService) {}
+
   @ApiBody({ type: LocalLoginDto })
   @Post('login')
   @UseGuards(LocalLoginGuard)
-  public async localLogin(@ReqUser() user: Payload): Promise<Payload> {
-    return user;
+  public async localLogin(@ReqUser() user: Payload): Promise<JwtSign> {
+    return this.auth.jwtSign(user);
   }
 
   @Get('logout')
