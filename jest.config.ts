59c592ea9cc966da60dc5c13faeaad72048a66ac
Dongjun Shin <53344349+wq0212@users.noreply.github.com>
2023-11-14 05:45:44 +0000

    fix: fix error that duplicate jest configurations

--- jest.config.ts
+++ jest.config.ts
@@ -1,7 +1,10 @@
 export default {
+  rootDir: 'src',
   preset: 'ts-jest',
   testEnvironment: 'node',
   testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.[jt]sx?$',
+  moduleFileExtensions: ['js', 'json', 'ts'],
   collectCoverage: true,
-  coverageDirectory: 'coverage',
+  collectCoverageFrom: ['**/*.(t|j)s'],
+  coverageDirectory: '../coverage',
 };
