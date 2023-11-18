9b0cb08bc9697da4594e6910821761fafeaf3899
Dongjun Shin <53344349+wq0212@users.noreply.github.com>
2023-11-18 07:53:11 +0000

    feat: Add exception in addRole function in user.service

--- src/shared/user/user.service.ts
+++ src/shared/user/user.service.ts
@@ -1,9 +1,4 @@
-import {
-  HttpException,
-  HttpStatus,
-  Injectable,
-  NotFoundException,
-} from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable, NotFoundException } from '@nestjs/common';
 import { QueryFailedError } from 'typeorm';
 import { Transactional } from 'typeorm-transactional';
 
@@ -37,31 +32,37 @@ export class UserService {
       for (let i = 0; i < roles?.length; i++) {
         const role_name = roles[i];
         const isSuccess = await this.role.addRoleToUser(role_name, user);
-        if (!isSuccess)
-          throw new NotFoundException(
-            `The role ${role_name} is not valid role`,
-          );
+        if (!isSuccess) throw new NotFoundException(`The role ${role_name} is not valid role`);
       }
       return true;
     } catch (error: unknown) {
       if (error instanceof QueryFailedError) {
         if (error?.driverError.code === MysqlErrorCode.ALREADY_USER)
-          throw new HttpException(
-            `User's Email already exists`,
-            HttpStatus.BAD_REQUEST,
-          );
+          throw new HttpException(`User's Email already exists`, HttpStatus.BAD_REQUEST);
       }
       if (error instanceof NotFoundException) {
         const message = error?.message;
-        throw new HttpException(message, HttpStatus.BAD_REQUEST);
+        throw new HttpException(message, HttpStatus.NOT_FOUND);
       }
       throw new HttpException('UNKNOWN ERROR', HttpStatus.BAD_REQUEST);
     }
   }
 
   public async addRole(data: addRoleDto): Promise<boolean> {
-    const user = await this.usersRepository.getByUserId(data.user_id);
-    if (!user) return false;
-    return await this.role.addRoleToUser(data.role_name, user);
+    try {
+      const user_id = data.user_id;
+      const role_name = data.role_name;
+      const user = await this.usersRepository.getByUserId(user_id);
+      if (!user) throw new NotFoundException(`User ID ${user_id} does NOT Exist`);
+      const isSuccess = await this.role.addRoleToUser(role_name, user);
+      if (!isSuccess) throw new NotFoundException(`The role ${role_name} is not valid role`);
+      return isSuccess;
+    } catch (error: unknown) {
+      if (error instanceof NotFoundException) {
+        const message = error?.message;
+        throw new HttpException(message, HttpStatus.NOT_FOUND);
+      }
+      throw new HttpException('UNKNOWN ERROR', HttpStatus.BAD_REQUEST);
+    }
   }
 }
