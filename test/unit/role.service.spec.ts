f9c6dcab097fb92364662d6a557de10329bb6c29
Dongjun Shin <53344349+wq0212@users.noreply.github.com>
2023-11-18 10:51:14 +0000

    test: Add all test about addRoleToUser function in role.service

---
+++ test/unit/role.service.spec.ts
@@ -0,0 +1,84 @@
+import { Test } from '@nestjs/testing';
+import { faker } from '@faker-js/faker';
+
+import { Role } from '#entities/role.entity';
+import { UserRole } from '#entities/user-role.entity';
+import { User } from '#entities/user.entity';
+
+import { RoleService, RolesRepository, UserRolesRepository } from '../../src/shared/role/providers';
+import { HttpException } from '@nestjs/common';
+
+describe('RoleService', () => {
+  let roleService: RoleService;
+  let rolesRepository: RolesRepository;
+  let userRolesRepository: UserRolesRepository;
+
+  const mockRolesRepository = {
+    findRoleByName: jest.fn(),
+  };
+
+  const mockUserRolesRepository = {
+    create: jest.fn(),
+  };
+
+  beforeEach(async () => {
+    const module = await Test.createTestingModule({
+      providers: [
+        RoleService,
+        {
+          provide: RolesRepository,
+          useValue: mockRolesRepository,
+        },
+        {
+          provide: UserRolesRepository,
+          useValue: mockUserRolesRepository,
+        },
+      ],
+    }).compile();
+    roleService = module.get<RoleService>(RoleService);
+    rolesRepository = module.get<RolesRepository>(RolesRepository);
+    userRolesRepository = module.get<UserRolesRepository>(UserRolesRepository);
+  });
+
+  it('should be defined', () => {
+    expect(roleService).toBeDefined();
+  });
+
+  describe('addRoleToUser', () => {
+    const role_name = 'User';
+    const user: User = {
+      user_id: 1,
+      username: faker.person.fullName(),
+      passwordHash: 'Hash!',
+      email: faker.internet.email(),
+      created_at: new Date(),
+      updated_at: new Date(),
+      roles: [],
+    };
+    const role: Role = {
+      role_id: 1,
+      role_name,
+      users: [new UserRole()],
+      created_at: new Date(),
+      updated_at: new Date(),
+    };
+    it('should assign a role to the user', async () => {
+      jest.spyOn(rolesRepository, 'findRoleByName').mockResolvedValue(role);
+      jest.spyOn(userRolesRepository, 'create').mockResolvedValue(new UserRole());
+
+      const result = await roleService.addRoleToUser(role_name, user);
+      expect(result).toBeTruthy();
+    });
+    it('should throw an exception for an invalid role', async () => {
+      jest.spyOn(rolesRepository, 'findRoleByName').mockResolvedValue(null);
+      jest.spyOn(userRolesRepository, 'create').mockResolvedValue(new UserRole());
+
+      await expect(async () => {
+        await roleService.addRoleToUser(role_name, user);
+      }).rejects.toThrow(HttpException);
+      await expect(async () => {
+        await roleService.addRoleToUser(role_name, user);
+      }).rejects.toThrow(`The role ${role_name} is not valid role`);
+    });
+  });
+});
