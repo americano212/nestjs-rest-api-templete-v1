29efd83ea280b199c845eddc5daadcd7ddd6d228
Dongjun Shin <53344349+wq0212@users.noreply.github.com>
2023-11-21 07:22:12 +0000

    feat: add enviroment value about kakao login

--- src/config/envs/production.ts
+++ src/config/envs/production.ts
@@ -35,4 +35,11 @@ export const config = {
   slack: {
     webhookUrl: process.env['SLACK_WEBHOOK_URL'],
   },
+  oauth: {
+    kakao: {
+      clientID: process.env['KAKAO_CLIENT_ID'],
+      clientSecret: process.env['KAKAO_CLIENT_SECRET'],
+      callbackURL: process.env['KAKAO_CALLBACK_URL'],
+    },
+  },
 };
