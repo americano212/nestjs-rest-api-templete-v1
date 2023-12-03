50371d838951cd0b0b8bc9d98f8ad38ea606f944
Dongjun Shin <53344349+wq0212@users.noreply.github.com>
2023-12-03 10:28:41 +0000

    style: Erase unused code

--- src/common/guards/roles.guard.ts
+++ src/common/guards/roles.guard.ts
@@ -3,7 +3,7 @@ import { Reflector } from '@nestjs/core';
 
 import { ROLES_KEY } from '../decorators';
 import { Role } from '../enums';
-import { AuthService, Payload } from 'src/auth';
+import { AuthService, Payload } from '../../../src/auth';
 
 @Injectable()
 export class RolesGuard implements CanActivate {
@@ -14,23 +14,16 @@ export class RolesGuard implements CanActivate {
 
   async canActivate(context: ExecutionContext): Promise<boolean> {
     const requiredRoles = this.reflector.getAllAndOverride<Role[]>(ROLES_KEY, [
-      context.getHandler(), // Method Roles
-      context.getClass(), // Controller Roles
+      context.getHandler(),
+      context.getClass(),
     ]);
-    console.log('requiredRoles', requiredRoles);
     if (!requiredRoles) return true;
 
     const { cookies } = context.switchToHttp().getRequest();
-    console.log('cookies', cookies);
-    console.log('AC', cookies.access_token);
     if (!cookies.access_token) return false;
 
     const payload: Payload = await this.auth.jwtVerify(cookies.access_token);
-    console.log('payload', payload);
-    console.log(
-      'isRole? : ',
-      requiredRoles.some((role) => payload.roles?.includes(role)),
-    );
+
     return requiredRoles.some((role) => payload.roles?.includes(role));
   }
 }
