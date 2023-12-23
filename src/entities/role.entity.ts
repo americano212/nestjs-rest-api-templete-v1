0c9a3d572100461e00dbb4fd1c2a58249b321fa2
Dongjun Shin <53344349+wq0212@users.noreply.github.com>
2023-12-23 05:25:41 +0000

    feat: Change role entity not extend coreEntitiy

--- src/entities/role.entity.ts
+++ src/entities/role.entity.ts
@@ -1,10 +1,10 @@
 import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
-import { CoreEntity } from './core.entity';
 import { IsInt, IsString } from 'class-validator';
+
 import { UserRole } from './user-role.entity';
 
 @Entity('role')
-export class Role extends CoreEntity {
+export class Role {
   @PrimaryGeneratedColumn({ type: 'int', unsigned: true, name: 'role_id' })
   @IsInt()
   public role_id!: number;
