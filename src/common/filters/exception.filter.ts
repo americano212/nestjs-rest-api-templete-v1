5f5b973ee411a07f0621bf031dc034f6ad4ec259
Dongjun Shin <53344349+wq0212@users.noreply.github.com>
2023-12-27 14:44:52 +0000

    feat: Update exception filter

--- src/common/filters/exception.filter.ts
+++ src/common/filters/exception.filter.ts
@@ -1,12 +1,16 @@
 import { Catch, ArgumentsHost, HttpStatus, HttpException, Logger } from '@nestjs/common';
-import { BaseExceptionFilter } from '@nestjs/core';
 import { Request, Response } from 'express';
+import { QueryFailedError } from 'typeorm';
+
+enum MysqlErrorCode {
+  ALREADY_EXIST = 'ER_DUP_ENTRY',
+}
 
 @Catch()
-export class ExceptionsFilter extends BaseExceptionFilter {
+export class ExceptionsFilter {
   private readonly logger: Logger = new Logger();
 
-  public override catch(exception: unknown, host: ArgumentsHost): void {
+  public catch(exception: unknown, host: ArgumentsHost): void {
     let args: unknown;
 
     const ctx = host.switchToHttp();
@@ -15,18 +19,35 @@ export class ExceptionsFilter extends BaseExceptionFilter {
     const statusCode = this.getHttpStatus(exception);
     const datetime = new Date();
 
-    super.catch(exception, host);
+    const message: string =
+      exception instanceof HttpException || exception instanceof QueryFailedError
+        ? exception.message
+        : 'UNKNOWN ERROR';
+
+    const errorResponse = {
+      code: statusCode,
+      timestamp: datetime,
+      path: req.url,
+      method: req.method,
+      message: message,
+    };
 
     if (statusCode >= HttpStatus.INTERNAL_SERVER_ERROR) {
-      this.logger.error({ datetime: datetime, err: exception, args: { req, res } });
+      this.logger.error({ err: errorResponse, args: { req, res } });
     } else {
-      this.logger.warn({ datetime: datetime, err: exception, args });
+      this.logger.warn({ err: errorResponse, args });
     }
+
+    res.status(statusCode).json(errorResponse);
   }
 
   private getHttpStatus(exception: unknown): HttpStatus {
-    return exception instanceof HttpException
-      ? exception.getStatus()
-      : HttpStatus.INTERNAL_SERVER_ERROR;
+    if (
+      exception instanceof QueryFailedError &&
+      exception.driverError.code === MysqlErrorCode.ALREADY_EXIST
+    ) {
+      return HttpStatus.BAD_REQUEST;
+    } else if (exception instanceof HttpException) return exception.getStatus();
+    else return HttpStatus.INTERNAL_SERVER_ERROR;
   }
 }
