d1710dabf31ac02392c8613140b7410efcb38425
Dongjun Shin <53344349+wq0212@users.noreply.github.com>
2023-12-28 06:25:49 +0000

    refactor: seperate interface that slack message

--- src/common/providers/slack.service.ts
+++ src/common/providers/slack.service.ts
@@ -3,24 +3,27 @@ import { IncomingWebhook } from '@slack/webhook';
 
 import { ConfigService } from './config.service';
 
+interface SlackMessage {
+  text: string;
+  channel: string;
+  username: string;
+  icon_emoji: string;
+}
+
 @Injectable()
 export class SlackService {
   constructor(private config: ConfigService) {}
 
   public async sendSlackMessage(
-    message: string,
-    channel?: string,
-    username?: string,
+    text: string,
+    channel: string = 'webhook-test',
+    username: string = 'slack-bot',
   ): Promise<boolean> {
     const url = this.config.get('slack.webhookUrl');
     const webhook = new IncomingWebhook(url);
-    // TODO make interface
-    const result = await webhook.send({
-      text: `${message}`,
-      channel: channel || 'webhook-test',
-      username: username || 'slack-bot',
-      icon_emoji: ':bell:',
-    });
+    const slackMessage: SlackMessage = { text, channel, username, icon_emoji: ':bell:' };
+
+    const result = await webhook.send(slackMessage);
     return result.text === 'ok' ? true : false;
   }
 }
