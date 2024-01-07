9cdf7f2ff73c680af5f01578c2a68689cfc7d55f
Dongjun Shin <53344349+wq0212@users.noreply.github.com>
2024-01-07 16:26:12 +0000

    feat: Seperate filter utility function

---
+++ src/common/filters/utils/get-http-status.ts
@@ -0,0 +1,13 @@
+import { HttpException, HttpStatus } from '@nestjs/common';
+import { QueryFailedError } from 'typeorm';
+import { MysqlErrorCode } from 'src/common/enums';
+
+export function getHttpStatus(exception: unknown): HttpStatus {
+  if (
+    exception instanceof QueryFailedError &&
+    exception.driverError.code === MysqlErrorCode.ALREADY_EXIST
+  ) {
+    return HttpStatus.CONFLICT;
+  } else if (exception instanceof HttpException) return exception.getStatus();
+  else return HttpStatus.INTERNAL_SERVER_ERROR;
+}
