c1c784a02f8050e1ca3ad4ffe48f5e27c828918a
Dongjun Shin <53344349+americano212@users.noreply.github.com>
2024-01-09 15:51:42 +0000

    feat: Add files url for download

--- src/domain/file/file.controller.ts
+++ src/domain/file/file.controller.ts
@@ -16,8 +16,9 @@ export class FileController {
   @Post('s3')
   @UseInterceptors(FileInterceptor('file'))
   async uploadFile(@UploadedFile() file: Express.Multer.File) {
+    const awsRegion = this.config.get('aws.region');
     AWS.config.update({
-      region: this.config.get('aws.region'),
+      region: awsRegion,
       credentials: {
         accessKeyId: this.config.get('aws.accessKey'),
         secretAccessKey: this.config.get('aws.secretKey'),
@@ -26,15 +27,18 @@ export class FileController {
     const BUCKET_NAME = this.config.get('aws.s3.bucketName');
     console.log('file', file);
     try {
+      const key = `${Date.now().toString()}-${file.originalname}`;
       const upload = await new AWS.S3()
         .putObject({
-          Key: `${Date.now() + file.originalname}`,
+          Key: key,
           Body: file.buffer,
           Bucket: BUCKET_NAME,
           ACL: 'public-read',
         })
         .promise();
+      const urlHead = `https://${BUCKET_NAME}.s3.${awsRegion}.amazonaws.com/`; // TODO replace CDN
       console.log('upload', upload);
+      console.log('url', urlHead + key);
     } catch (error) {
       console.log('error', error);
     }
